c7519ee2ad473f435f4923c16fd3b5f8dd254072
lisichkaggg <mudarisowash@gmail.com>
2021-10-04 16:22:27 +0300

    Added configuration for acceptSuggestion

--- extension.js
+++ extension.js
@@ -1,46 +1,61 @@
-const vscode = require('vscode')
+const vscode = require("vscode");
 
-function colonize (option) {
-  var editor = vscode.window.activeTextEditor
-  if (!editor) return
+async function colonize(option) {
+  var editor = vscode.window.activeTextEditor;
+  if (!editor) return;
 
-  vscode.commands.executeCommand('acceptSelectedSuggestion').then(() => {
-    var lineIndex = editor.selection.active.line
-    var lineObject = editor.document.lineAt(lineIndex)
-    var lineLength = lineObject.text.length
+  var acceptSuggestion = vscode.workspace
+    .getConfiguration("colonize")
+    .get("acceptSuggestion");
 
-    if (lineObject.text.charAt(lineLength - 1) !== ';' && !lineObject.isEmptyOrWhitespace) {
-      var insertionSuccess = editor.edit((editBuilder) => {
-        editBuilder.insert(new vscode.Position(lineIndex, lineLength), ';')
-      })
+  if (acceptSuggestion) {
+    await vscode.commands.executeCommand("acceptSelectedSuggestion");
+  }
 
-      if (!insertionSuccess) return
+  var lineIndex = editor.selection.active.line;
+  var lineObject = editor.document.lineAt(lineIndex);
+  var lineLength = lineObject.text.length;
+
+  if (
+    lineObject.text.charAt(lineLength - 1) !== ";" &&
+    !lineObject.isEmptyOrWhitespace
+  ) {
+    var insertionSuccess = editor.edit((editBuilder) => {
+      editBuilder.insert(new vscode.Position(lineIndex, lineLength), ";");
+    });
+
+    if (!insertionSuccess) return;
+  }
+
+  if (option === "hold") return;
+
+  option === "endline"
+    ? vscode.commands.executeCommand("cursorEnd")
+    : vscode.commands.executeCommand("editor.action.insertLineAfter");
+}
+
+function activate(context) {
+  var endLineDisposable = vscode.commands.registerCommand(
+    "colonize.endline",
+    () => {
+      colonize("endline");
     }
+  );
 
-    if (option === 'hold') return
+  var holdDisposable = vscode.commands.registerCommand("colonize.hold", () => {
+    colonize("hold");
+  });
 
-    option === 'endline'
-      ? vscode.commands.executeCommand('cursorEnd')
-      : vscode.commands.executeCommand('editor.action.insertLineAfter')
-  })
+  var newLineDisposable = vscode.commands.registerCommand(
+    "colonize.newline",
+    () => {
+      colonize("newline");
+    }
+  );
+
+  context.subscriptions.push(endLineDisposable);
+  context.subscriptions.push(newLineDisposable);
+  context.subscriptions.push(holdDisposable);
 }
 
-function activate (context) {
-  var endLineDisposable = vscode.commands.registerCommand('colonize.endline', () => {
-    colonize('endline')
-  })
-
-  var holdDisposable = vscode.commands.registerCommand('colonize.hold', () => {
-    colonize('hold')
-  })
-
-  var newLineDisposable = vscode.commands.registerCommand('colonize.newline', () => {
-    colonize('newline')
-  })
-
-  context.subscriptions.push(endLineDisposable)
-  context.subscriptions.push(newLineDisposable)
-  context.subscriptions.push(holdDisposable)
-}
-
-exports.activate = activate
+exports.activate = activate;
